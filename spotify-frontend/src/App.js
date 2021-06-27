27a600b6782905107406a765b90b9f61fbb87e9b
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-06-27 13:55:34 -0700

    set up basic app components

---
+++ spotify-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import routes from './config/routes'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ spotify-frontend/src/components/Playlist.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Playlist = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Playlist
